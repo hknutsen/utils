05a6a54fcf8270d355b8b8213bbcd118b47051b5
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-03-16 09:58:34 +0100

    Mount read-only filesystem (#1)
    * Mount media disk
    
    * Mount read-only filesystem
    
    * Consistent spaces
    
    * Remove nesting
    
    * Move script into scripts directory
    
    * Make script executable
    
    * Create mount point parent directories

---
+++ scripts/mount_readonly_filesystem.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+# Mounts/unmounts filesystem with read-only permissions.
+#
+# Usage:
+# - Mount filesystem with label "Media": ./mount_readonly_filesystem.sh -l Media
+# - Unmount filesystem with label "Media": ./mount_readonly_filesystem.sh -l Media -u
+
+label=""
+unmount=false
+
+while getopts l:u opt; do
+  case $opt in
+    l)
+      label="$OPTARG"
+      ;;
+    u)
+      unmount=true
+      ;;
+    *)
+      exit 1
+      ;;
+  esac
+done
+
+if [[ -z "$label" ]]; then
+  echo "-l <LABEL> must be specified."
+  exit 1
+fi
+
+file_system=$(findfs LABEL="$label")
+user_name=$(whoami)
+mount_point="/run/media/$user_name/$label"
+
+if [[ "$unmount" == true ]]; then
+  echo "Unmounting $file_system from $mount_point"
+  sudo umount "$mount_point"
+  exit 0
+fi
+
+if [[ ! -d "$mount_point" ]]; then
+  echo "Creating directory $mount_point"
+  sudo mkdir -p "$mount_point"
+fi
+
+echo "Mounting $file_system to $mount_point"
+sudo mount -r "$file_system" "$mount_point"
