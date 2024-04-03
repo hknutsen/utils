1e720d549faecba40bcdc296692ad6a1232b6ad4
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-04-03 18:31:03 +0200

    Update resampling script

---
+++ scripts/audio/resample_flac.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+# Reads audio from lossless FLAC files in the source directory and resamples
+# to 16-bit/44.1kHz (CD quality) FLAC files in the output directory.
+#
+# Prereqs:
+#   sox
+#
+# Arguments:
+#   Source directory
+#   Output directory (relative to source directory)
+#
+# Usage:
+#   ./resample_flac.sh /mnt/Data/Music/ ~/Music/
+
+set -eu
+
+source_dir=$1
+output_dir=$2
+
+cd "$source_dir" || exit
+
+if [[ ! -d "$output_dir" ]]; then
+  mkdir "$output_dir"
+fi
+
+readarray -t flac_files < <(find . -name "*.flac")
+
+for flac_file in "${flac_files[@]}"; do
+  output_file="$output_dir/$flac_file"
+  if [[ -f "$output_file" ]]; then
+    echo "$output_file already exists"
+    continue
+  fi
+
+  parent_dir=$(dirname "$output_file")
+  if [[ ! -d "$parent_dir" ]]; then
+    mkdir -p "$parent_dir"
+  fi
+
+  sox "$flac_file" -b 16 "$output_file" rate "44.1k" dither
+done
