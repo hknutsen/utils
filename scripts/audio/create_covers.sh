dff27fc89accc3de66555ca7d11af88dba79e227
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-04-10 17:43:34 +0200

    Update source directory for cover creation script

--- scripts/audio/create_covers.sh
+++ scripts/audio/create_covers.sh
@@ -1,44 +1,40 @@
 #! /bin/bash
 
-# Convert images in cover art directory to "cover.jpg" files.
-# Source cover art path: /mnt/Data/Pictures/Cover Art/<album_name>.jpg
-# Destination album path: /mnt/Data/Music/<album_name>
+# Converts all "cover.jpg" files in the source music directory to "cover_med.jpg" files suitable for embedding.
+#
+# Prereqs:
+#   ImageMagick
+#
+# Arguments:
+#   Source directory
+#
+# Usage:
+#   ./create_covers.sh /mnt/Data/Music
 
-set -e
+set -eu
 
-source_dir="/mnt/Data/Pictures/Cover Art"
-music_dir="/mnt/Data/Music"
+source_dir=$1
 
-cd "$source_dir" || exit
+cd "$source_dir"
 
-if [[ ! -d "$source_dir" ]]; then
-  echo "Source directory $source_dir does not exist"
-  exit 1
-fi
+readarray -t cover_files < <(find . -name "cover.jpg")
 
-if [[ ! -d "$music_dir" ]]; then
-  echo "Music directory $music_dir does not exist"
-  exit 1
-fi
+for input_file in "${cover_files[@]}"; do
+  echo "Converting $input_file"
 
-readarray -t source_files < <(find -- *.jpg)
+  output_dir=$(dirname "$input_file")
 
-for source_file in "${source_files[@]}"; do
-  album_name="${source_file/".jpg"/""}"
-  album_dir="$music_dir/$album_name"
+  # Convert cover art to small, medium and large sizes.
+  # I currently do not need the small and large cover art sizes, but have decided to leave them in for future reference.
+  # The small cover art size could be useful for thumbnails.
+  # The large cover art size could be useful for larger displays, for example TVs.
+  # The medium cover art size works well for mobile displays, which is my use case.
 
-  if [[ ! -d "$album_dir" ]]; then
-    echo "Album directory $album_dir does not exist"
-    exit 1
-  fi
+  # By default, the ImageMagick convert tool estimates an appropriate quality level based on the input image.
+  # If an estimate cannot be made, quality is set to 92 instead (ref: https://www.imagemagick.org/script/command-line-options.php#quality).
+  # We want a consistent quality level for all album covers, so we explicitly set the quality to 92.
 
-  cover_file="$album_dir/cover.jpg"
-
-  if [[ -f "$cover_file" ]] && [[ ! "$1" == "-f" ]]; then
-    echo "Cover file $cover_file already exists"
-  else
-    echo "Converting $source_file to $cover_file"
-    convert "$source_file" -resize 600x600 -quality 92 "$cover_file"
-    # 92 is the default quality, but it's set explicitly to clarify what's being done (resize with quality 92)
-  fi
+  # convert "$input_file" -resize 300x300 -quality 92 "$output_dir/cover_small.jpg"
+  convert "$input_file" -resize 600x600 -quality 92 "$output_dir/cover_med.jpg"
+  # convert "$input_file" -resize 1200x1200 -quality 92 "$output_dir/cover_large.jpg"
 done
