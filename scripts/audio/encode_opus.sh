21cc52c5916dbbe656022f1ba9fae718372fa719
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-04-30 19:18:52 +0200

    wip

--- scripts/audio/encode_opus.sh
+++ scripts/audio/encode_opus.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-# Decodes audio from lossless FLAC files in the source directory and encodes to lossy Opus files in the target directory.
-#
-# Prereqs:
+# Decodes audio from lossless FLAC files in the source directory and
+# encodes to lossy Opus files in the target directory.
+# Prerequisites:
 #   parallel
 #   opus-tools
 #   rsgain
@@ -15,22 +15,8 @@
 
 set -eu
 
-################################################################################
-# Convert input file to Opus.
-# Globals:
-#   OUTPUT_DIR
-# Arguments:
-#   Input file, a name.
-#   Output file, a name.
-# Outputs:
-#   Writes output file path to stdout
-################################################################################
-function convert_to_opus {
-  output_file="${OUTPUT_DIR}/$2"
-  # According to the Xiph.Org Foundation (developers of Opus), "Opus at 128 KB/s (VBR) is pretty much transparent".
-  # Ref: https://wiki.xiph.org/Opus_Recommended_Settings#Recommended_Bitrates (2024/04/03)
-  opusenc --bitrate 128 --vbr --quiet "$1" "${output_file}"
-  realpath "${output_file}"
+function doit {
+  opusenc --bitrate 128 --vbr --quiet "$INPUT_DIR/$1" "$OUTPUT_DIR/$2"
 }
 
 if [[ ! -d "$1" ]]; then
@@ -40,16 +26,17 @@ fi
 SOURCE_DIR=$(realpath "$1")
 readonly SOURCE_DIR
 
+# TODO: check that TARGET_DIR is empty to prevent overriding existing files
 if [[ ! -d "$2" ]]; then
   mkdir -p "$2"
 fi
 TARGET_DIR=$(realpath "$2")
 readonly TARGET_DIR
 
-# Export constants and functions to environment
+# Export variables and functions to environment
 export INPUT_DIR
 export OUTPUT_DIR
-export -f convert_to_opus
+export -f doit
 
 # Find album directories.
 cd "${SOURCE_DIR}"
@@ -57,8 +44,8 @@ readarray -t ALBUM_DIRS < <(find . -maxdepth 1 -type d | sort)
 readonly ALBUM_DIRS
 
 for ALBUM_DIR in "${ALBUM_DIRS[@]}"; do
-  # Find FLAC files in album directory.
-  # If album directory doesn't contain any FLAC files, skip and continue loop.
+  # Set input directory and find FLAC files.
+  # If input directory doesn't contain any FLAC files, skip.
   INPUT_DIR=$(realpath "${SOURCE_DIR}/${ALBUM_DIR}")
   cd "$INPUT_DIR"
   readarray -t flac_files < <(find . -maxdepth 1 -type f -name "*.flac" | sort)
@@ -72,12 +59,11 @@ for ALBUM_DIR in "${ALBUM_DIRS[@]}"; do
     mkdir -p "${OUTPUT_DIR}"
   fi
 
-  # Convert FLAC files to Opus.
-  echo "Converting FLAC files in '$INPUT_DIR' to Opus in '$OUTPUT_DIR'"
-  readarray -t opus_files < <(parallel 'convert_to_opus {} {.}.opus' ::: "${flac_files[@]}")
-
-  # Calculate track and album gain, and write RFC 7845 standard tags.
-  # Ref: https://datatracker.ietf.org/doc/html/rfc7845#section-5.2.1 (2024/04/16)
-  echo "Calculating track and album gain in '$OUTPUT_DIR'"
-  rsgain custom --album --tagmode=i --opus-mode=s --quiet "${opus_files[@]}"
+  # According to the Xiph.Org Foundation (developers of Opus), "Opus at 128 KB/s (VBR) is pretty much transparent".
+  # Ref: https://wiki.xiph.org/Opus_Recommended_Settings#Recommended_Bitrates (2024/04/03)
+  echo "Converting FLAC files in '$INPUT_DIR' to Opus in '$OUTPUT_DIR'..."
+  parallel "doit {} {.}.opus" ::: "${flac_files[@]}"
 done
+
+echo "Calculating gain in '$TARGET_DIR'..."
+rsgain easy -m MAX "$TARGET_DIR"
