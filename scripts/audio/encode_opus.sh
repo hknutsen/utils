c9119fed5c47bf13b15cf5e5e70af5efec112bba
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-04-30 20:27:39 +0200

    Encode Opus in parallel (#11)
    * Encode Opus in parallel
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * refactor
    
    * Sort arrays
    
    * Write progress
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * refactor

--- scripts/audio/encode_opus.sh
+++ scripts/audio/encode_opus.sh
@@ -1,71 +1,60 @@
 #! /bin/bash
 
-# Decodes audio from lossless FLAC files in the source directory and encodes to lossy Opus files in the output directory.
-#
-# Prereqs:
+# Decodes audio from lossless FLAC files in the input directory and
+# encodes to lossy Opus files in the output directory.
+# Prerequisites:
+#   parallel
 #   opus-tools
 #   rsgain
-#
+# Arguments:
+#   Input directory, a path.
+#   Output directory, a path.
 # Usage:
-#   ./encode_opus.sh
+#   ./encode_opus.sh <INPUT_DIR> <OUTPUT_DIR>
+#   ./encode_opus.sh /mnt/Data/Music ~/Music/opus
 
 set -eu
 
-source_dir="/mnt/Data/Music"
-output_dir="$HOME/Music/opus"
+function doit {
+  input_file="${INPUT_DIR}/$1"
+  output_file="${OUTPUT_DIR}/$2"
 
-if [[ ! -d "$source_dir" ]]; then
-  echo "Source directory '$source_dir' does not exist"
+  # Ensure directory exists.
+  dir=$(dirname "${output_file}")
+  if [[ ! -d "${dir}" ]]; then
+    mkdir -p "${dir}"
+  fi
+
+  # According to the Xiph.Org Foundation (developers of Opus), "Opus at 128 KB/s (VBR) is pretty much transparent".
+  # Ref: https://wiki.xiph.org/Opus_Recommended_Settings#Recommended_Bitrates (2024/04/03)
+  opusenc --bitrate 128 --vbr --quiet "${input_file}" "${output_file}"
+}
+
+# Ensure the input directory exists.
+# Export it to the environment.
+if [[ ! -d "$1" ]]; then
+  echo "Source directory '$1' does not exist"
   exit 1
 fi
+INPUT_DIR=$(realpath "$1")
+export INPUT_DIR
 
-if [[ ! -d "$output_dir" ]]; then
-  echo "Output directory '$output_dir' does not exist"
-  exit 1
+# Ensure output directory exists.
+# Export it to the environment.
+if [[ ! -d "$2" ]]; then
+  mkdir -p "$2"
 fi
+OUTPUT_DIR=$(realpath "$2")
+export OUTPUT_DIR
 
-cd "$source_dir"
+# Export the function to the environment.
+export -f doit
 
-readarray -t album_dirs < <(find . -mindepth 1 -maxdepth 1 -type d | sort)
+# Convert FLAC files in input the directory to Opus files in the output directory.
+echo "Converting FLAC files in '$INPUT_DIR' to Opus files in '$OUTPUT_DIR'"
+cd "${INPUT_DIR}"
+find . -type f -name "*.flac" | sort | parallel --progress 'doit {} {.}.opus'
 
-for album_dir in "${album_dirs[@]}"; do
-  cd "$source_dir/$album_dir"
-
-  readarray -t flac_files < <(find . -maxdepth 1 -type f -name "*.flac" | sort)
-
-  if [[ "${#flac_files[@]}" -eq 0 ]]; then
-    echo "Album directory '$album_dir' does not contain any FLAC files"
-    continue
-  fi
-
-  target_dir="$output_dir/$album_dir"
-
-  if [[ ! -d "$target_dir" ]]; then
-    mkdir -p "$target_dir"
-  fi
-
-  opus_files=()
-
-  for flac_file in "${flac_files[@]}"; do
-    opus_file="$target_dir/${flac_file/".flac"/".opus"}"
-
-    if [[ -f "$opus_file" ]]; then
-      echo "Opus file '$opus_file' already exists"
-      continue
-    fi
-
-    # According to the Xiph.Org Foundation (developers of Opus), "Opus at 128 KB/s (VBR) is pretty much transparent".
-    # Ref: https://wiki.xiph.org/Opus_Recommended_Settings#Recommended_Bitrates (2024/04/03)
-    echo "Converting '$flac_file' to '$opus_file'"
-    opusenc --bitrate 128 --vbr --quiet "$flac_file" "$opus_file"
-
-    opus_files+=("$opus_file")
-  done
-
-  if [[ "${#opus_files[@]}" -gt 0 ]]; then
-    # Calculate track and album gain, and write RFC 7845 standard tags.
-    # Ref: https://datatracker.ietf.org/doc/html/rfc7845#section-5.2.1 (2024/04/16)
-    echo "Calculating gain for '$target_dir'"
-    rsgain custom --album --tagmode=i --opus-mode=s --quiet "${opus_files[@]}"
-  fi
-done
+# Calculate gain for Opus files in the output directory.
+echo "Calculating gain for Opus files in '$OUTPUT_DIR'"
+rsgain easy -m MAX "$OUTPUT_DIR"
