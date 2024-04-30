8ee81ec51c640c9b8d87e44c6d5cc607cb128e85
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-04-30 11:03:44 +0200

    wip

--- scripts/audio/encode_opus.sh
+++ scripts/audio/encode_opus.sh
@@ -12,21 +12,31 @@
 
 set -eu
 
+doit() {
+  # According to the Xiph.Org Foundation (developers of Opus), "Opus at 128 KB/s (VBR) is pretty much transparent".
+  # Ref: https://wiki.xiph.org/Opus_Recommended_Settings#Recommended_Bitrates (2024/04/03)
+  opusenc --bitrate 128 --vbr --quiet "$1" "$2"
+  echo "$2"
+}
+
 if [[ ! -d "$1" ]]; then
   echo "Directory '$1' does not exist"
   exit 1
 fi
 
+# TODO: clarify this section
+SOURCE_DIR=$(realpath "$1")
+cd "$SOURCE_DIR"
+readarray -t album_dirs < <(find . -type d -maxdepth 1)
+
 if [[ ! -d "$2" ]]; then
   mkdir -p "$2"
 fi
 
-SOURCE_DIR=$(realpath "$1")
+# TODO: vlarify this section
 OUTPUT_DIR=$(realpath "$2")
 
-cd "$SOURCE_DIR"
-readarray -t album_dirs < <(find . -type d -maxdepth 1)
-
+# TODO: clarify this section
 for album_dir in "${album_dirs[@]}"; do
   cd "$SOURCE_DIR/$album_dir"  
   readarray -t flac_files < <(find . -type f -name "*.flac" -maxdepth 1)
@@ -41,11 +51,8 @@ for album_dir in "${album_dirs[@]}"; do
     mkdir -p "$target_dir"
   fi
 
-  # According to the Xiph.Org Foundation (developers of Opus), "Opus at 128 KB/s (VBR) is pretty much transparent".
-  # Ref: https://wiki.xiph.org/Opus_Recommended_Settings#Recommended_Bitrates (2024/04/03)
-  parallel opusenc --bitrate 128 --vbr --quiet "{}" "$target_dir/{.}.opus" ::: "${flac_files[@]}"
+  readarray -t opus_files < <(parallel doit "{}" "$target_dir/{.}.opus" ::: "${flac_files[@]}")
 
-  readarray -t opus_files < <(find "$target_dir" -type f -name "*.opus" -maxdepth 1)
   if [[ "${#opus_files[@]}" -gt 0 ]]; then
     # Calculate track and album gain, and write RFC 7845 standard tags.
     # Ref: https://datatracker.ietf.org/doc/html/rfc7845#section-5.2.1 (2024/04/16)
