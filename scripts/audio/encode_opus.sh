c9ac2df7a693f71eef7a5223cc43f5b5cfed8b19
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-05-02 20:48:01 +0200

    Update encode_opus.sh

--- scripts/audio/encode_opus.sh
+++ scripts/audio/encode_opus.sh
@@ -29,23 +29,7 @@ function doit {
   # According to the Xiph.Org Foundation (developers of Opus),
   # "Opus at 128 KB/s (VBR) is pretty much transparent".
   # Ref: https://wiki.xiph.org/Opus_Recommended_Settings (2024/04/03)
-  #
-  # According to the Opus specification, gain must be stored in the
-  # "Output Gain" field in the ID header. Media players should apply this output
-  # gain by default. Additional track and album gain should be stored in the
-  # "R128_TRACK_GAIN" and "R128_ALBUM_GAIN" fields in the comment header. Media
-  # players should apply the track and album gain in addition to the output
-  # gain. The reference Opus encoder stores album gain in the "Output Gain"
-  # field and additional track gain in the "R128_TRACK_GAIN" field. It is
-  # implicit that the value of the "R128_ALBUM_GAIN" field is 0, however the
-  # lack of an explicit "R128_ALBUM_GAIN" field results in some media players
-  # falling back to applying the additional track gain when only album gain is
-  # preferred. Explicitly set the value of the "R128_ALBUM_GAIN" field to 0 to
-  # prevent this behavior.
-  # Ref: https://datatracker.ietf.org/doc/html/rfc7845 (2024/05/02)
-  opusenc --bitrate 128 --vbr \
-    --comment "R128_ALBUM_GAIN=0" \
-    --quiet "${input_file}" "${output_file}"
+  opusenc --bitrate 128 --vbr --quiet "${input_file}" "${output_file}"
 }
 
 # Ensure the input directory exists.
@@ -68,6 +52,4 @@ export -f doit
 
 # Convert FLAC files to Opus files in parallel child processes.
 cd "${INPUT_DIR}"
-find . -type f -name "*.flac" \
-  | sort \
-  | parallel --progress 'doit {} {.}.opus'
+find . -type f -name "*.flac" | sort | parallel --progress 'doit {} {.}.opus'
