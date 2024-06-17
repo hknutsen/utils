048982131b2ffbceb0ffb5303b2eef0995cb545a
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-06-17 17:35:48 +0200

    Skip FLAC files already encoded using latest FLAC encoder

--- scripts/audio/encode_flac.sh
+++ scripts/audio/encode_flac.sh
@@ -15,12 +15,28 @@
 
 set -eu
 
+function doit {
+  flac_file="$FLAC_DIR/$1"
+
+  file_encoding=$(metaflac --show-vendor-tag "$flac_file")
+  encoder_version=$(flac --version)
+  if [[ "${file_encoding,,}" == *"${encoder_version,,}"* ]]; then
+    # If input FLAC file already encoded using latest FLAC encoder, skip.
+    exit 0
+  fi
+
+  flac --best --quiet --force "$flac_file"
+}
+
 # Ensure the input directory exists.
 if [[ ! -d "$FLAC_DIR" ]]; then
   echo "Input directory '$FLAC_DIR' does not exist"
   exit 1
 fi
 
+# Export function, allowing child processes to inherit it.
+export -f doit
+
 # Re-encode FLAC files in parallel child processes.
 cd "$FLAC_DIR"
-find . -name '*.flac' -type f | sort | parallel --progress 'flac --best --force {}'
+find . -name '*.flac' -type f | sort | parallel --progress 'doit {}'
