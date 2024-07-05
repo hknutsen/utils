c342dfdd0127b17e0caac3d8537adb1325187830
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-07-05 20:54:18 +0200

    Go back to VS Code instead of NeoVim

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -20,13 +20,17 @@ packages=(
   rsgain     # ReplayGain tagging utility
 
   # Development tools
-  neovim # Code editor
-  gh     # GitHub CLI
+  code # VS Code
+  gh   # GitHub CLI
 )
 
 flatpaks=(
   com.spotify.Client # Music streaming
 )
+
+# Add VS Code repository
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 # Install packages
 sudo dnf install "${packages[@]}"
