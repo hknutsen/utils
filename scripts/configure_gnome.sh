13ba412d9fd02da753a553e690891281c02eadc8
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-07-05 21:13:29 +0200

    Increase cursor size

--- scripts/configure_gnome.sh
+++ scripts/configure_gnome.sh
@@ -7,3 +7,5 @@ gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
 
 # Increase font scaling to 125% (since I think fonts are too small on 27" 1440p displays)
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
+# # Increase cursor size for same reason as above
+# gsettings set org.gnome.desktop.interface cursor-size 32
