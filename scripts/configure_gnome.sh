7275aba6885d773cdf414973fab1d8c55854c74c
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-07-05 22:54:58 +0200

    Remove commented commands

--- scripts/configure_gnome.sh
+++ scripts/configure_gnome.sh
@@ -7,8 +7,6 @@ gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
 
 # Increase font scaling to 125% (since I think fonts are too small on 27" 1440p displays)
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
-# # Increase cursor size for same reason as above
-# gsettings set org.gnome.desktop.interface cursor-size 32
 
 # Always show accessibility menu in the top bar
 gsettings set org.gnome.desktop.a11y always-show-universal-access-status true
