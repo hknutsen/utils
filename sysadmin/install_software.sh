14b13000bfc85d51fbbf0079ffa2889dcedc6636
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-07-06 12:49:09 +0200

    Wait for Kernel modules to complete installation

--- sysadmin/install_software.sh
+++ sysadmin/install_software.sh
@@ -22,7 +22,7 @@ flatpaks=(
   com.spotify.Client
 )
 
-sudo dnf update -y
+sudo dnf update
 
 # Enable RPM Fusion repositories.
 # Required for installing proprietary software, such as Nvidia drivers.
@@ -30,12 +30,21 @@ sudo dnf update -y
 sudo dnf install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
 sudo dnf install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
-# Add VS Code repository
+# Add VS Code repository.
+# Ref: https://code.visualstudio.com/docs/setup/linux
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-# Install packages
+# Install packages.
 sudo dnf install "${packages[@]}"
 
-# Install flatpaks
+# Install flatpaks.
 flatpak install flathub "${flatpaks[@]}"
+
+# Wait for Kernel modules to complete building...
+until modinfo -F version nvidia
+do
+  echo 'Building Nvidia Kernel module...'
+done
+echo 'Nvidia Kernel module build complete!'
+echo 'Restart your computer to finish installation of drivers.'
