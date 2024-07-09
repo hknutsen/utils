a4d5a4a6ae9083f125bcccd0c0dadc3d3c99f2b3
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-07-09 11:14:26 +0200

    Update configure_system.sh

--- sysadmin/configure_system.sh
+++ sysadmin/configure_system.sh
@@ -5,11 +5,12 @@ set -eu
 # Set system hostname
 hostname henrik-desktop
 
+################################################################################
+# CONFIGURE GNOME (DESKTOP ENVIRONMENT)
+################################################################################
+
 # Disable mouse acceleration
 gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
-
-# # Increase font scaling to 125% (since I think fonts are too small on 27" 1440p displays)
-# gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
 
 # Always show accessibility menu in the top bar
 gsettings set org.gnome.desktop.a11y always-show-universal-access-status true
