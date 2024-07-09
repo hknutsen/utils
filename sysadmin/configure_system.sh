d1303f0347aa49186f07107597676cd7d8d05561
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-07-09 15:19:46 +0200

    Update configure_system.sh

--- sysadmin/configure_system.sh
+++ sysadmin/configure_system.sh
@@ -14,3 +14,6 @@ gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
 
 # Always show accessibility menu in the top bar
 gsettings set org.gnome.desktop.a11y always-show-universal-access-status true
+
+# Increase text scaling to 125%
+# gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
