573f5e65f5d1338bb02af35a16b4bb7465622d33
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-07-09 11:20:05 +0200

    Update install_software.sh

--- sysadmin/install_software.sh
+++ sysadmin/install_software.sh
@@ -21,7 +21,13 @@ sudo dnf install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfr
 # Add VS Code repository.
 # Ref: https://code.visualstudio.com/docs/setup/linux
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+echo "[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" |
+sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 ################################################################################
 # INSTALL PACKAGES
