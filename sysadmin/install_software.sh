c7c788c176d43271b37db4f45d5ff3fcc1d01684
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-07-09 11:19:01 +0200

    Update install_software.sh

--- sysadmin/install_software.sh
+++ sysadmin/install_software.sh
@@ -2,6 +2,31 @@
 
 set -eu
 
+################################################################################
+# UPDATE SYSTEM SOFTWARE
+################################################################################
+
+sudo dnf upgrade
+
+################################################################################
+# ADD SOFTWARE REPOSITORIES
+################################################################################
+
+# Enable RPM Fusion repositories.
+# Required for installing proprietary software, such as Nvidia drivers.
+# Ref: https://docs.fedoraproject.org/en-US/quick-docs/rpmfusion-setup/
+sudo dnf install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+sudo dnf install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+
+# Add VS Code repository.
+# Ref: https://code.visualstudio.com/docs/setup/linux
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+################################################################################
+# INSTALL PACKAGES
+################################################################################
+
 packages=(
   deja-dup
   thunderbird
@@ -18,28 +43,21 @@ packages=(
   foliate
 )
 
+sudo dnf install "${packages[@]}"
+
+################################################################################
+# INSTALL FLATPAKS
+################################################################################
+
 flatpaks=(
   com.bitwarden.desktop
   com.spotify.Client
 )
 
-sudo dnf upgrade
-
-# Enable RPM Fusion repositories.
-# Required for installing proprietary software, such as Nvidia drivers.
-# Ref: https://docs.fedoraproject.org/en-US/quick-docs/rpmfusion-setup/
-sudo dnf install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
-sudo dnf install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-
-# Add VS Code repository.
-# Ref: https://code.visualstudio.com/docs/setup/linux
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-
-# Install packages.
-sudo dnf install "${packages[@]}"
-
-# Install flatpaks.
 flatpak install flathub "${flatpaks[@]}"
 
-echo 'Restart your computer to finish installation of drivers.'
+################################################################################
+# FINISH INSTALLATION
+################################################################################
+
+echo 'Restart your computer to finish installation of software.'
