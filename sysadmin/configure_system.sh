312326ce8ad9830198962d9966458ac0e6175171
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-07-10 17:43:47 +0200

    Update configure_system.sh

--- sysadmin/configure_system.sh
+++ sysadmin/configure_system.sh
@@ -13,6 +13,7 @@ sudo hostname henrik-desktop
 # CONFIGURE GNOME (DESKTOP ENVIRONMENT)
 ################################################################################
 
+# Tell dconf to synchronize the binary database with a plain text keyfile in ${XDG_CONFIG_HOME}/dconf/user.txt.
 # Ref: https://wiki.gnome.org/Projects/dconf/SystemAdministrators
 echo 'service-db:keyfile/user
 user-db:user
@@ -20,4 +21,3 @@ system-db:local
 system-db:site
 system-db:distro' > /etc/dconf/profile/user
 
-echo 'Restart your computer to finish configuration of GNOME.'
