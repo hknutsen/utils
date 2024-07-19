2815a33adfab31a494becff33211d34c19449c5a
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-07-19 14:53:22 +0200

    Remove Bitwarden
    Use Browser extension instead

--- sysadmin/install_software.sh
+++ sysadmin/install_software.sh
@@ -78,7 +78,6 @@ sudo dnf install "${packages[@]}"
 ################################################################################
 info 'Installing flatpaks...'
 flatpaks=(
-  com.bitwarden.desktop
   com.spotify.Client
 )
 flatpak install flathub "${flatpaks[@]}"
